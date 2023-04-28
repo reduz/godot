b0ac8726c96740bddd872fe0ccabeabc3984ad7b
Juan Linietsky <reduzio@gmail.com>
2023-04-28 13:15:36 +0200

    Redo how the remote filesystem works
    Instead of reading files over the network, the new version uses a local file cache and only updates files when it changes.
    
    The original remote filesystem was created 14 years ago, when ethernet was faster than hard drives or even flash. Also, mobile devices have a very small amount of storage.
    Nowadays, this is no longer the case so the approach is changed to using a persistent cache in the target device.

--- core/variant/variant_utility.cpp
+++ core/variant/variant_utility.cpp
@@ -803,7 +803,7 @@ struct VariantUtilityFunctions {
 		print_line_rich(s);
 		r_error.error = Callable::CallError::CALL_OK;
 	}
-
+#undef print_verbose
 	static inline void print_verbose(const Variant **p_args, int p_arg_count, Callable::CallError &r_error) {
 		if (OS::get_singleton()->is_stdout_verbose()) {
 			String s;
